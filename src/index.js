5a2a66c8a7dd6c0b89712e0652c4d19bbe6671ad
Mark Gowen <mark.gowen78@gmail.com>
2022-11-08 18:09:29 -0800

    Challenge 1 completed and tested

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+function onPageLoad() {
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(data => {
+        data.message.forEach((url) => {
+            let dogImgContainer = document.querySelector("#dog-image-container")
+            let dogImg = document.createElement("img")
+            dogImgContainer.append(dogImg)
+            dogImg.src = url
+        })
+    })
+}
+
+onPageLoad()
+    
+    
+
+
